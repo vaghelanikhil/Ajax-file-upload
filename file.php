47d86ce189555166087e85bf050e862038f9179c
vaghelanikhil <vaghelanikhil@users.noreply.github.com>
2016-11-28 18:10:00 +0530

    Update file.php

--- file.php
+++ file.php
@@ -1,11 +1,11 @@
 <?php
 
-    if ( 0 < $_FILES['myfile']['error'] ) {
-        echo 'Error: ' . $_FILES['myfile']['error'] . '<br>';
+foreach($_FILES['myfile']['name'] as $key=>$val){
+        //upload and stored images
+        $target_dir = "uploads/";
+        $target_file = $target_dir.$_FILES['myfile']['name'][$key];
+        if(move_uploaded_file($_FILES['myfile']['tmp_name'][$key],$target_file)){
+            $images_arr[] = $target_file;
+        }
     }
-    else {
-        move_uploaded_file($_FILES['myfile']['tmp_name'], 'uploads/' . $_FILES['myfile']['name']);
-		echo "success";
-    }
-
 ?>
